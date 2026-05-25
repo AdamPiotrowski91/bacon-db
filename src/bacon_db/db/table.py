438ef15b3cc14be8d115ba3d3530bb0d55c1623f
AdamPiotrowski91 <adam.piotrowski@10g.pl>
2026-05-25 22:06:23 +0200

    FE - single table setup

---
+++ src/bacon_db/db/table.py
@@ -0,0 +1,20 @@
+from .. import json as j
+from ..json import Data  # explicitly imported type
+
+# region Helpers
+
+
+class TableHandlerError(RuntimeError):
+    """Error Raised by `DatabaseHandler` class during runtime."""
+
+
+# endregion
+
+# region Implementation
+
+
+class TableHandler:
+    pass
+
+
+# endregion
